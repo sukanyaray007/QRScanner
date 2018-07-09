5df3c21de260e1b96a9a44b733857c5af9923f4e
Sukanya Ray <suku_the_smart@yahoo.co.in>
2018-07-09 13:03:16 +0530

    QR format

--- www/js/index.js
+++ www/js/index.js
@@ -35,7 +35,7 @@
             torchOn: true, // Android, launch with the torch switched on (if available)
             prompt : "Place a barcode inside the scan area", // Android
             resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
-            formats : default, // default: all but PDF_417 and RSS_EXPANDED "QR_CODE,PDF_417"
+            formats : "QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED 
             orientation : "landscape", // Android only (portrait|landscape), default unset so it rotates with the device
             disableAnimations : true, // iOS
             disableSuccessBeep: false // iOS
